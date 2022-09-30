a1db852d83852b4aab170595c88e55b0d2752e40
Ma-Ko-dev <Mathias.Kowitz@gmx.de>
2022-09-30 14:31:27 +0200

    Basic functionality

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A simple Text to Speech converter using googles gTTS module in Python. Supports several Languages.
 
-Work in progress and currently not working at all. Just the basic design and some functionality present.
+Work in progress. Basic Design and functionality already working.
 
 Tested only on Windows.
 </br></br>
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from tkinter import *
 from tkinter import filedialog
+from gtts import gTTS
 import gtts.lang
 import os
+
 
 # constants
 WIDTH = 600
 HEIGHT = 500
 PADDINGX = 20
 PADDINGY = 20
+# TODO: Add a config file to save the last Save location and maybe other stuff.
 FILE_PATH = "Not set"
+# TODO: Add a Version Check once the initial release is out.
+# OWNER = " Ma-Ko-dev"
+# REPO = "TTS_w_GUI"
+# URL = f"https://api.github.com/repos/{OWNER}/{REPO}/releases/latest"
+#
+# version_check = requests.get(URL)
+# print(version_check)
+tts_langs = gtts.tts.tts_langs()
 
 
 # Button functions
@@ -24,10 +35,20 @@ def open_dir():
     os.startfile(FILE_PATH)
 
 
+def text_to_speech():
+    # TODO: Add a file name with current date and time.
+    global FILE_PATH
+    text = text_value.get(1.0, "end-1c")
+    tts_lang = [k for k, v in tts_langs.items() if v == lang.get()][0]
+    tts_slow = slow.get()
+    tts_obj = gTTS(text=text, lang=tts_lang, slow=tts_slow)
+    tts_obj.save(f"{FILE_PATH}/text_to_speech.mp3".replace('"', ""))
+    # TODO: Add a notification when the files was created.
+
+
 # GUI CREATION
 root = Tk()
 root.minsize(WIDTH, HEIGHT)
-# root.config(padx=PADDINGX, pady=PADDINGY, bg="DeepPink")
 root.config(padx=PADDINGX, pady=PADDINGY)
 root.resizable(width=False, height=False)
 root.title("Text to Speech")
@@ -37,14 +58,14 @@ save_label = Label(text=FILE_PATH)
 save_label.grid(row=2, column=0, columnspan=2, padx=2, pady=10, sticky="w")
 
 # option menu
-tts_langs = gtts.tts.tts_langs()
 lang = StringVar()
 lang.set("German")
 dropdown = OptionMenu(root, lang, *tts_langs.values())
 dropdown.grid(row=0, column=0, padx=2, pady=2, sticky="ew")
 
 # checkbutton
-check_slow = Checkbutton(root, text="Slow?")
+slow = BooleanVar()
+check_slow = Checkbutton(root, text="Slow?", variable=slow)
 check_slow.grid(row=0, column=1, padx=2, pady=2, sticky="ew")
 
 # button
@@ -52,7 +73,7 @@ location_button = Button(text="Set Save Location", command=set_location)
 location_button.grid(row=1, column=0, padx=2, sticky="ew")
 open_button = Button(text="Open Folder", command=open_dir)
 open_button.grid(row=1, column=1, padx=2, sticky="ew")
-convert_button = Button(text="Convert")
+convert_button = Button(text="Convert", command=text_to_speech)
 convert_button.grid(row=4, column=0, columnspan=3, pady=5, sticky="ew")
 
 # text
